931925776945e3131a10f3394b388bfa1474393e
Daniel Galvis <85359306+DanielGalvis-Dev@users.noreply.github.com>
2024-08-10 10:04:06 -0500

    Delete pruebas directory

--- pruebas/script.js
+++
@@ -1,8 +0,0 @@
-const recuperarDatos = async () => {
-  const res = await fetch("?controller=motor&action=graficas");
-  const respuesta = await res.json();
-  console.log(respuesta);
-  return respuesta;
-};
-
-recuperarDatos();
